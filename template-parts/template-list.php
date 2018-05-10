4df19ba1438e9669d73804f4d6eba3a8a3e1f7c2
Robert Paul <rpauldesign@gmail.com>
2018-05-10 22:10:19 +0100

    Added correct escaping

--- template-parts/template-list.php
+++ template-parts/template-list.php
@@ -1,6 +1,8 @@
 <article class="list">
 	<h3 class="list__title">
-		<a class="list__link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		<a class="list__link" href="<?php esc_url_e( get_the_permalink() ); ?>"><?php esc_html_e( get_the_title() ); ?></a>
 	</h3>
-	<time class="list__date"><span class="entypo-clock"></span><?php echo get_the_date(); ?></time>
+	<time class="list__date">
+		<span class="entypo-clock"></span><?php esc_html_e( get_the_date() ); ?>
+	</time>
 </article>

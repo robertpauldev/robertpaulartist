47c19e958d3def522ae9afba87bedf26e4dec5c6
Robert Paul <rpauldesign@gmail.com>
2018-05-10 21:52:01 +0100

    Added esc_url_e() function

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -1,6 +1,24 @@
 <?php
 
 /**
+ * Adds the URL escaping shorthand that WordPress so badly needs.
+ */
+if ( false === function_exists( 'esc_url_e' ) ) {
+
+	/**
+	 * Adds the URL escaping shorthand that WordPress so badly needs.
+	 *
+	 * @param string $url The URL to be escaped.
+	 * @param [type] $protocols ...
+	 * @param string $_context ...
+	 * @return void
+	 */
+	function esc_url_e( $url, $protocols = null, $_context = 'display' ) {
+		echo apply_filters( 'esc_url_e', esc_url( $url, $protocols, $_context ) );
+	}
+}
+
+/**
  * Returns Facebook OpenGraph data based on data type.
  *
  * @param string $type Sets which OpenGraph property to define.

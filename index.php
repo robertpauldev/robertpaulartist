6ea6a2eed82f49e1c5f7f721381b2b876ceb61a1
Robert Paul <rpauldesign@gmail.com>
2018-07-16 21:09:34 +0100

    Added content area to homepage

--- index.php
+++ index.php
@@ -2,17 +2,24 @@
 
 <section class="wrap">
 	<?php
-		if ( have_posts() ) {
-			while ( have_posts() ) {
+		/** Loop */
+		if ( have_posts() ) :
+			while ( have_posts() ) :
 				the_post();
+	?>
 
+	<div class="entry__cms">
+		<?php the_content(); ?>
+	</div>
+
+	<?php
 				/** Homepage */
-				if ( is_front_page() ) {
+				if ( is_front_page() ) :
 					get_template_part( 'template-parts/template', 'slider' );
 					get_template_part( 'template-parts/template', 'flex' );
-				}
-			}
-		}
+				endif;
+			endwhile;
+		endif;
 	?>
 </section>
 

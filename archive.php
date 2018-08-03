244833ec086cae281ae1acbe0082705cbbc871e4
Robert Paul <rpauldesign@gmail.com>
2018-08-03 19:07:18 +0100

    Updated template part calls

--- archive.php
+++ archive.php
@@ -4,7 +4,7 @@
 	<h1 class="page-title"><?php the_archive_title(); ?></h1>
 	<?php
 		if ( is_tag( 'pencil' ) || is_tag( 'ink' ) ) {
-			get_template_part( 'template-parts/template', 'flex' );
+			get_template_part( 'template-parts/template', 'grid' );
 		} else {
 			get_template_part( 'template-parts/template', 'list' );
 		}

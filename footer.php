d53f162de7e8a02aea3ecd06100545bbc0a1c993
Robert Paul <rpauldesign@gmail.com>
2019-06-19 19:29:24 +0100

    Footer template & LESS improvements

--- footer.php
+++ footer.php
@@ -9,11 +9,16 @@ defined( 'ABSPATH' ) or die();
 		</main>
 		<footer class="footer">
 			<div class="wrap">
-				<div class="copyright">&copy;<?php echo esc_html( date( 'Y' ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?></div>
-				<?php
-				echo rpa_social( 'facebook' );
-				echo rpa_social( 'instagram' );
-				?>
+				<div class="copyright">
+					<?php get_template_part( 'template-parts/template', 'logo' ); ?>
+					<span>&copy;<?php echo esc_html( date( 'Y' ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?></span>
+				</div>
+				<div class="social">
+					<?php
+					echo rpa_social( 'facebook' );
+					echo rpa_social( 'instagram' );
+					?>
+				</div>
 			</div>
 		</footer>
 	</body>

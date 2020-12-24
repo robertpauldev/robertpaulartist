377754c28b4dd31fc255acef992f2c606b5ff5d0
Robert Paul <rpauldesign@gmail.com>
2020-12-24 12:59:34 +0000

    Added native lazy loading to post thumbnail html

--- functions.php
+++ functions.php
@@ -148,3 +148,19 @@ function rpa_disable_emojis() {
 	remove_action( 'wp_print_styles', 'print_emoji_styles' );
 }
 add_action( 'init', 'rpa_disable_emojis' );
+
+/**
+ * Add `loading="lazy"` attribute to images output by the_post_thumbnail().
+ *
+ * @param string       $html              The post thumbnail HTML.
+ * @param int          $post_id           The post ID.
+ * @param string       $post_thumbnail_id The post thumbnail ID.
+ * @param string|array $size              The post thumbnail size. Image size or array of width and height values (in that order). Default 'post-thumbnail'.
+ * @param string       $attr              Query string of attributes.
+ * 
+ * @return string
+ */
+function rpa_thumbnail_lazy_loading( $html, $post_id, $post_thumbnail_id, $size, $attr ) {
+	return str_replace( '<img', '<img loading="lazy"', $html );
+}
+add_filter( 'post_thumbnail_html', 'rpa_thumbnail_lazy_loading', 10, 5 );
